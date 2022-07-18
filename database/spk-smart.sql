671729c14a793742283fc99abe0f6c16b8bb9f87
harun5897 <harungb025@gmail.com>
2022-07-18 11:38:30 +0700

    add database

---
+++ database/spk-smart.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 18, 2022 at 06:37 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spk-smart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabelkriteria`
+--
+
+CREATE TABLE `tabelkriteria` (
+  `idKriteria` int(9) NOT NULL,
+  `namaKriteria` varchar(255) NOT NULL,
+  `bobotKriteria` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabelkriteria`
+--
+
+INSERT INTO `tabelkriteria` (`idKriteria`, `namaKriteria`, `bobotKriteria`) VALUES
+(1, 'komputer', 35),
+(2, 'pendidikan', 30),
+(3, 'pengalaman', 20),
+(4, 'kendaraan', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabelpenilaian`
+--
+
+CREATE TABLE `tabelpenilaian` (
+  `idPenilaian` int(9) NOT NULL,
+  `idPeserta` int(9) NOT NULL,
+  `kriteriaKomputer` int(9) NOT NULL,
+  `kriteriaPendidikan` int(9) NOT NULL,
+  `kriteriaPengalaman` int(9) NOT NULL,
+  `kriteraKendaraan` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabelpenilaian`
+--
+
+INSERT INTO `tabelpenilaian` (`idPenilaian`, `idPeserta`, `kriteriaKomputer`, `kriteriaPendidikan`, `kriteriaPengalaman`, `kriteraKendaraan`) VALUES
+(11, 4, 11, 11, 11, 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabelpeserta`
+--
+
+CREATE TABLE `tabelpeserta` (
+  `idPeserta` int(9) NOT NULL,
+  `namaDepan` varchar(255) NOT NULL,
+  `namaBelakang` varchar(255) NOT NULL,
+  `nik` varchar(255) NOT NULL,
+  `tanggalLahir` varchar(255) NOT NULL,
+  `jenisKelamin` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `kontak` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabelpeserta`
+--
+
+INSERT INTO `tabelpeserta` (`idPeserta`, `namaDepan`, `namaBelakang`, `nik`, `tanggalLahir`, `jenisKelamin`, `agama`, `alamat`, `email`, `kontak`) VALUES
+(4, 'joko ', 'susilo', '123456789', '1993-08-08', 'laki-laki', 'islam', 'Yogyakarta ', 'joko@gmail.com', '+62 081360271959');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabeluser`
+--
+
+CREATE TABLE `tabeluser` (
+  `idUser` int(9) NOT NULL,
+  `namaUser` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `kataSandi` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabeluser`
+--
+
+INSERT INTO `tabeluser` (`idUser`, `namaUser`, `email`, `kataSandi`, `role`) VALUES
+(1, 'Admin', 'admin@gmail.com', '123456', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabelkriteria`
+--
+ALTER TABLE `tabelkriteria`
+  ADD PRIMARY KEY (`idKriteria`);
+
+--
+-- Indexes for table `tabelpenilaian`
+--
+ALTER TABLE `tabelpenilaian`
+  ADD PRIMARY KEY (`idPenilaian`);
+
+--
+-- Indexes for table `tabelpeserta`
+--
+ALTER TABLE `tabelpeserta`
+  ADD PRIMARY KEY (`idPeserta`);
+
+--
+-- Indexes for table `tabeluser`
+--
+ALTER TABLE `tabeluser`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tabelkriteria`
+--
+ALTER TABLE `tabelkriteria`
+  MODIFY `idKriteria` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tabelpenilaian`
+--
+ALTER TABLE `tabelpenilaian`
+  MODIFY `idPenilaian` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tabelpeserta`
+--
+ALTER TABLE `tabelpeserta`
+  MODIFY `idPeserta` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tabeluser`
+--
+ALTER TABLE `tabeluser`
+  MODIFY `idUser` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
